d7c2da89d79d2cc9581e8097e91fa29165816f1c
Eri Sawada <esawada2003@gmail.com>
2025-09-19 11:53:33 +0900

    add somem files for example

--- examples/ffmpeg/server.py
+++ examples/ffmpeg/server.py
@@ -1,15 +1,18 @@
-from mcp.server.fastmcp import FastMCP
-import ffmpeg
 import re
 import time
 import traceback
+
+import ffmpeg
+from mcp.server.fastmcp import FastMCP
 
 
 mcp = FastMCP("FFmpeg")
 
 
 @mcp.tool()
-def run_ffmpeg(input_file: str, refs: int, qcomp: float, qdiff: int, me_range: int, x264opts: str) -> str:
+def run_ffmpeg(
+    input_file: str, refs: int, qcomp: float, qdiff: int, me_range: int, x264opts: str
+) -> str:
     """Run ffmpeg and returns the value of SSIM (Strucutural SIMilarity) and the elapsed time.
 
     Arguments:
@@ -23,30 +26,32 @@ def run_ffmpeg(input_file: str, refs: int, qcomp: float, qdiff: int, me_range: i
     """
     start = time.time()
     try:
-        enc = ffmpeg.input(input_file, t=50)  # Use only the first 50 seconds instead of the entire video for demo purposes.
+        enc = ffmpeg.input(
+            input_file, t=50
+        )  # Use only the first 50 seconds instead of the entire video for demo purposes.
         enc = enc.output(
-            '-', 
-            f='null', 
-            tune='ssim', 
-            ssim=1, 
-            an=None, 
-            vcodec='libx264', 
-            s='320x240', 
-            vb='100k',
-            refs=refs, 
-            qcomp=qcomp, 
-            qdiff=qdiff, 
+            "-",
+            f="null",
+            tune="ssim",
+            ssim=1,
+            an=None,
+            vcodec="libx264",
+            s="320x240",
+            vb="100k",
+            refs=refs,
+            qcomp=qcomp,
+            qdiff=qdiff,
             me_range=me_range,
-            x264opts=x264opts
+            x264opts=x264opts,
         )
         _, stderr = enc.run(capture_stderr=True)
 
         elapsed = time.time() - start
-        ssim_mean = re.search('SSIM Mean Y:([0-9.]+)', stderr.decode('utf-8')).group(1)
+        ssim_mean = re.search("SSIM Mean Y:([0-9.]+)", stderr.decode("utf-8")).group(1)
 
         return f"SSIM (mean): {ssim_mean}, Elapsed: {elapsed} seconds"
     except Exception as e:
-        stacktrace = '\n'.join(traceback.format_tb(e.__traceback__))
+        stacktrace = "\n".join(traceback.format_tb(e.__traceback__))
         return f"Error: {e}, {traceback}: {traceback}"
 
 
